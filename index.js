6ad8fe545b54034203c93efc5b0afaa9c1786e6a
Brandy <133482446+pm-brandy-pics@users.noreply.github.com>
2024-05-13 23:08:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function onClick() {
+    alert("Alerted successfully")
+}
+
+var video = document.getElementById("background");
+video.play();
+
+var audio = document.getElementById("song");
+audio.play();
